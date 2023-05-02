2bdb75b470e4c0863b88c7c491fc81e340bde5f8
Alexandru Pele <pelealexandru@gmil.com>
2023-05-02 11:29:06 +0300

    Remove connect button and redirect directly to Vault app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,27 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import {useEffect} from 'react';
 import {
     AuthType,
-    SismoConnectButton,
-    SismoConnectClientConfig,
-    SismoConnectResponse
-} from "@sismo-core/sismo-connect-react";
-
-const inter = Inter({ subsets: ['latin'] })
+    SismoConnect,
+    SismoConnectClientConfig
+} from '@sismo-core/sismo-connect-client';
 
 const sismoConnectConfig: SismoConnectClientConfig = {
-    appId: "0xd083e52833d6f2843a85a2a1650a789a" //
+    appId: '0x9d7a55d7871a6bb4761534e72d69f1f9'
 };
 
+const sismoConnect = SismoConnect(sismoConnectConfig);
+
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>sismo-web</title>
-        <meta name="description" content="sismo-web" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <SismoConnectButton
-            config={sismoConnectConfig} //
-            auths={[{authType:AuthType.VAULT}]}
-            onResponse={(response: SismoConnectResponse) => {
-                console.log(response);
-            }}
-        />
-      </main>
-    </>
-  )
+    useEffect(() => {
+        const sismoConnectResponse = sismoConnect.getResponse();
+
+        if (sismoConnectResponse == null) {
+            sismoConnect.request({
+                auths: [{authType: AuthType.VAULT}]
+            });
+        } else {
+            const proofData = sismoConnectResponse.proofs.at(0)?.proofData || "undefined";
+            window.location.replace(`sismo://proof/${proofData}`);
+        }
+    });
 }
